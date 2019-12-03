28b8e96761a9968f0cedf9e01b364170f9a28266
BillyCoding <gustavo.rocha.191066@hotmail.com>
2019-12-03 18:34:40 -0300

    Estado e cidade adicionados

--- src/main/java/com/gustavo/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavo/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gustavo.cursomc.domain.Categoria;
+import com.gustavo.cursomc.domain.Cidade;
+import com.gustavo.cursomc.domain.Estado;
 import com.gustavo.cursomc.domain.Produtos;
 import com.gustavo.cursomc.repositories.CategoriaRepository;
+import com.gustavo.cursomc.repositories.CidadeRepository;
+import com.gustavo.cursomc.repositories.EstadoRepository;
 import com.gustavo.cursomc.repositories.ProdutosRepository;
 
 @SpringBootApplication
@@ -17,9 +21,13 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired	
 	private CategoriaRepository repo;
-	
 	@Autowired
 	private ProdutosRepository prod;
+	@Autowired
+	private CidadeRepository cidadeRepo;
+	@Autowired
+	private EstadoRepository estadoRepo;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -32,12 +40,22 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat2 = new Categoria(null, "Smartphone");
 		Produtos pro1 = new Produtos(null, "IdeaPad 320", 1700.00);
 		Produtos pro2 = new Produtos(null, "Moto G6 Indigo", 1250.00);
+		Estado esta1 = new Estado(null, "SãoPaulo");
+		Cidade cida1 = new Cidade(null, "Osasco", esta1);
+		Cidade cida2 = new Cidade(null, "Barueri", esta1);
+		Cidade cida3 = new Cidade(null, "Jandira", esta1);
+		Cidade cida4 = new Cidade(null, "Carapicuíba", esta1);
 		
 		cat1.getProdutos().addAll(Arrays.asList(pro1));
 		cat2.getProdutos().addAll(Arrays.asList(pro2));
 		
 		pro1.getCategorias().addAll(Arrays.asList(cat1));
 		pro2.getCategorias().addAll(Arrays.asList(cat2));
+		
+		esta1.getCidade().addAll(Arrays.asList(cida1));
+		
+		estadoRepo.saveAll(Arrays.asList(esta1));
+		cidadeRepo.saveAll(Arrays.asList(cida1, cida2, cida3, cida4));
 		
 		repo.saveAll(Arrays.asList(cat1, cat2));
 		prod.saveAll(Arrays.asList(pro1, pro2));
---
+++ src/main/java/com/gustavo/cursomc/domain/Estado.java
@@ -0,0 +1,36 @@
+package com.gustavo.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+public @Data class Estado implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidade = new ArrayList<>();
+
+	public Estado() {}
+	public Estado(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	
+}
